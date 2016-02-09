fd45dda5cb0295f0c206a9752a2553bfc077ca3d
Tina Chung <tchung1995@gmail.com>
2016-02-09 23:00:02 +0000

    Testing heroku.

--- app.js
+++ app.js
@@ -44,7 +44,7 @@ if ('development' == app.get('env')) {
 app.get('/', index.view);
 //app.get('/home', home.showHome);
 app.get('/home', home.view);
-app.get('/home', home.showHome);
+//app.get('/home', home.showHome);
 app.get('/friends', friends.view);
 app.get('/myprofile',myprofile.view);
 app.get('/settings', settings.view);
