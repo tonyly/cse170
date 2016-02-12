3071304b27a48f19d7eb97f2131d9f6278367454
Tina Chung <tchung1995@gmail.com>
2016-02-12 05:40:08 +0000

    inbox stuff

--- routes/inbox.js
+++ routes/inbox.js
@@ -1,6 +1,14 @@
 var data = require('../data.json');
 
+
 exports.view = function(req, res){
 	console.log(data);
 	res.render('inbox', data);
 };
+
+/*
+exports.view = function(req, res){
+	var title = req.params.title;
+	var msg = req.params.body;
+	req.json()
+}*/
